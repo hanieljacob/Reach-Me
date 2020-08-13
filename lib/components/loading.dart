e460ca1cc1c51a10a3739a8a00f2898f0d8bf074
ajkmr7 <ajayyasodha@gmail.com>
2020-08-13 15:46:18 +0530

    Added Profile, Search Users

---
+++ lib/components/loading.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircularProgressIndicator(
+            backgroundColor: Colors.white,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Loading',
+            style: TextStyle(
+                color: Colors.blue, fontWeight: FontWeight.w400, fontSize: 18),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/models/User.dart
@@ -0,0 +1,7 @@
+class User {
+  String name;
+  String photoUrl;
+  List posts;
+  String uid;
+  User({this.name, this.photoUrl, this.posts, this.uid});
+}
