1e90af0f8af66af19c524bdd570ac5151651cb11
Haniel Edward Jacob <haniel20008@gmail.com>
2020-08-21 21:28:28 +0530

    Following Page

--- lib/screens/FollowingPage.dart
+++ lib/screens/FollowingPage.dart
@@ -6,7 +6,8 @@ import 'package:reach_me/services/database.dart';
 class FollowingPage extends StatefulWidget {
   final List followers;
   final String uid;
-  FollowingPage({this.followers,this.uid});
+  final String title;
+  FollowingPage({this.followers,this.uid,@required this.title});
   @override
   _FollowingPageState createState() => _FollowingPageState();
 }
@@ -27,6 +28,7 @@ class _FollowingPageState extends State<FollowingPage> {
   void getFollowers(List follower) {
     db.getFollowers(follower).then((value) => setState((){
       followers = value;
+      print(value);
       unfollow();
     }));
   }
@@ -37,6 +39,9 @@ class _FollowingPageState extends State<FollowingPage> {
       firstTime = false;
     }
     return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title.toString()),
+      ),
       body:  ListView.builder(itemCount: widget.followers.length,itemBuilder: (BuildContext context,int index){
         return followers.length==0?SizedBox.shrink():boolList[index]?ListTile(
           onTap: () {},
@@ -45,7 +50,7 @@ class _FollowingPageState extends State<FollowingPage> {
             NetworkImage(followers[index].photoUrl),
           ),
           title: Text(followers[index].name),
-          trailing: FlatButton(
+          trailing: widget.title.toString() == "Likes"?SizedBox.shrink():FlatButton(
             textColor: Colors.white,
             color: Colors.blue,
             child: Text('Unfollow'),
